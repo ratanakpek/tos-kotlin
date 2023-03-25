9df0d514d3ead48970e699ef80a219c9f5fbb316
ratanak <ratanakpek088@gmail.com>
2023-03-25 16:03:48 +0700

    Modifier & Card

--- app/src/main/java/com/example/designpatterninkotlinjava/MainComposeActivity.kt
+++ app/src/main/java/com/example/designpatterninkotlinjava/MainComposeActivity.kt
@@ -3,42 +3,137 @@ package com.example.designpatterninkotlinjava
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainComposeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HelloWorldColumn()
+            val painter = painterResource(id = R.drawable.shuttlecock)
+            CardDemo(
+                painter,
+                "Hello World",
+                "RatanakPek"
+            )
+        }
+    }
+}
+
+
+//@Preview(showBackground = true)
+//@Composable
+//fun DefaultPreview() {
+//    CardDemo()
+//}
+
+@Composable
+fun CardDemo(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(16.dp),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Card(
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(16.dp),
+            elevation = 4.dp
+        ) {
+            Box(modifier = Modifier.height(200.dp)) {
+                Image(
+                    painter = painter,
+                    contentDescription = contentDescription,
+                    contentScale = ContentScale.Crop,
+                )
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(
+                            Brush.verticalGradient(
+                                colors = listOf(Color.Transparent, Color.Black),
+                                startY = 300f
+                            )
+                        )
+                )
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(12.dp),
+                    contentAlignment = Alignment.BottomStart
+                ) {
+                    Text(title, style = TextStyle(color = Color.White, fontSize = 16.sp))
+                }
+            }
         }
     }
 }
 
 @Composable
-fun HelloWorldColumn() {
-    Column(
-        Modifier.background(shape = RectangleShape, color = Color.Blue)
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+fun ModifierDemo() {
+    Row(
+        Modifier
+            .background(shape = RectangleShape, color = Color.Red)
+            .fillMaxHeight(0.5f)
+            .fillMaxWidth()
+            .padding(16.dp)
+            .border(4.dp, Color.Magenta)
+            .padding(8.dp)
+            .border(4.dp, Color.Green)
+            .padding(4.dp)
+            .border(4.dp, Color.DarkGray)
+            .padding(4.dp)
+            .border(4.dp, Color.White)
+            .clickable { },
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        Text("Hello")
-        Text("World")
+        Text(
+            "Hello",
+            Modifier
+                .background(Color.Blue)
+                .padding(4.dp),
+            color = Color.White
+        )
+        Spacer(modifier = Modifier.width(4.dp))
+        Text(
+            "World",
+            Modifier
+                .background(Color.Yellow)
+                .padding(4.dp)
+        )
+        Spacer(modifier = Modifier.width(4.dp))
+        Text(
+            "Ratanak",
+            Modifier
+                .background(Color.Green)
+                .padding(4.dp)
+        )
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    HelloWorldColumn()
 }
